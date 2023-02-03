7a0ef850dab2c3de49bc71160151c451d2788656
Mayur Sapaliga <msapaliga@collabware.com>
2023-02-03 11:55:21 -0800

    Add project files.

---
+++ Services.Membership/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Services.Membership
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        public string PrintMembership() {
+
+            string message = "I'm Membership Service";
+            Console.WriteLine(message);
+            return message;
+        }
+    }
+}
